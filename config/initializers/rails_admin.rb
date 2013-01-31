b3310235ce32a6c2bc8ddcff698a924811ad539e
Dave Ekhaus <dekhaus@mac.com>
2013-01-30 18:24:23 -0800

    Added Quiz model and configured RailsAdmin to manage it

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,12 +7,16 @@ RailsAdmin.config do |config|
   ################  Global configuration  ################
 
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Rails3 Bootstrap Devise Cancan', 'Admin']
+  config.main_app_name = ['Quizster', 'Admin']
   # or for a more dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
   # RailsAdmin may need a way to know who the current user is]
   config.current_user_method { current_user } # auto-generated
+
+
+  config.model 'Quiz' do
+  end
 
   # If you want to track changes on your models:
   # config.audit_with :history, 'User'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130131021319) do
+ActiveRecord::Schema.define(:version => 20130131021918) do
+
+  create_table "quizzes", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
