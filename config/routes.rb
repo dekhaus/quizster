ae8a238443a3a68e93c09a7c405f3a3d7beaaf1c
Dave Ekhaus <dekhaus@mac.com>
2013-01-30 18:15:11 -0800

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails3BootstrapDeviseCancan::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users
+  resources :users
+end
